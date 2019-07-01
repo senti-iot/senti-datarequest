abbc15b85c713667bfd6510484dfc0c0be39a2ee
Mikkel Broberg <mb@webhouse.dk>
2019-07-01 14:49:59 +0200

    Fix on service deploy

--- scripts/service-deploy.sh
+++ scripts/service-deploy.sh
@@ -2,24 +2,24 @@
 
 if [[ "$1" == "master" ]]; then 
 	echo
-	echo Deploying Senti subscriptions $1 ... 
-	rsync -r --quiet $2/ deploy@rey.webhouse.net:/srv/nodejs/senti/services/subscriptions/production
+	echo Deploying Senti datarequest $1 ... 
+	rsync -r --quiet $2/ deploy@rey.webhouse.net:/srv/nodejs/senti/services/datarequest/production
 	echo
-	echo Restarting Senti subscriptions service: $1 ... 
-	ssh deploy@rey.webhouse.net 'sudo /srv/nodejs/senti/services/subscriptions/production/scripts/service-restart.sh master'
+	echo Restarting Senti datarequest service: $1 ... 
+	ssh deploy@rey.webhouse.net 'sudo /srv/nodejs/senti/services/datarequest/production/scripts/service-restart.sh master'
 	echo
-	echo Deployment to Senti subscriptions $1 and restart done!
+	echo Deployment to Senti datarequest $1 and restart done!
 	exit 0
 fi 
 
 if [[ "$1" == "dev" ]]; then 
 	echo
-	echo Deploying Senti subscriptions $1 ... 
-	rsync -r --quiet $2/ deploy@rey.webhouse.net:/srv/nodejs/senti/services/subscriptions/development
+	echo Deploying Senti datarequest $1 ... 
+	rsync -r --quiet $2/ deploy@rey.webhouse.net:/srv/nodejs/senti/services/datarequest/development
 	echo
-	echo Restarting Senti subscriptions service: $1 ... 
-	ssh deploy@rey.webhouse.net 'sudo /srv/nodejs/senti/services/subscriptions/development/scripts/service-restart.sh dev'
+	echo Restarting Senti datarequest service: $1 ... 
+	ssh deploy@rey.webhouse.net 'sudo /srv/nodejs/senti/services/datarequest/development/scripts/service-restart.sh dev'
 	echo
-	echo Deployment to Senti subscriptions $1 and restart done!
+	echo Deployment to Senti datarequest $1 and restart done!
 	exit 0
 fi
--- scripts/service-restart.sh
+++ scripts/service-restart.sh
@@ -2,19 +2,19 @@
 # chmod 700 api-restart.sh
 
 if [[ "$1" == "master" ]]; then 
-	npm install --prefix /srv/nodejs/senti/services/subscriptions/production
-	systemctl restart senti-subscriptions.service
+	npm install --prefix /srv/nodejs/senti/services/datarequest/production
+	systemctl restart senti-datarequest.service
 	# Senti Slack Workspace
-	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti subscriptions MASTER updated and restarted!"}' https://hooks.slack.com/services/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
+	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti datarequest MASTER updated and restarted!"}' https://hooks.slack.com/services/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
 	echo
 	exit 0
 fi
 
 if [[ "$1" == "dev" ]]; then 
-	npm install --prefix /srv/nodejs/senti/services/subscriptions/development
-	systemctl restart senti-subscriptions-dev.service
+	npm install --prefix /srv/nodejs/senti/services/datarequest/development
+	systemctl restart senti-datarequest-dev.service
 	# Senti Slack Workspace
-	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti subscriptions DEV updated and restarted!"}' https://hooks.slack.com/services/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
+	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti datarequest DEV updated and restarted!"}' https://hooks.slack.com/services/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
 	echo
 	exit 0
 fi
